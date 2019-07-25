fa93df2c85b1b34f3d3c1cda9b5cb0b259e2a352
Tjibber Donker <tjibbed@gmail.com>
2019-07-25 10:46:21 +0200

    Adjusted checkDate function to fit with workflow
    I changed the function to reflect our standard variable nomenclature (pID, hID, Adate, Ddate), and added options to provide the coloumn names if different from the standard.

--- R/DataManagement.R
+++ R/DataManagement.R
@@ -8,10 +8,11 @@
 #'
 #' @param base (data.table).
 #'     A patient discharge database, in the form of a data.table. The data.table should have at least the following columns:
-#'         patientID (character)
-#'         hospitalID (character)
-#'         discharge (POSIXct, but character can be converted to POSIXct)
-#'         admission (POSIXct, but character can be converted to POSIXct)
+#'         pID: patientID (character)
+#'         hID: hospitalID (character)
+#'         Adate: admission date (POSIXct, but character can be converted to POSIXct)
+#'         Ddate: discharge date (POSIXct, but character can be converted to POSIXct)
+#'         
 #'
 #' @param convertDates boolean indicating if dates need to be converted to POSIXct if they are not
 #' @param dateFormat character giving the input format of the date character string
@@ -20,7 +21,11 @@
 #' @param deleteErrors character indicating the way incorrect records should be deleted: 
 #'                     "record" deletes just the incorrect record
 #'                     "patient" deletes all records of each patient with one or more incorrect records.
-#'                     
+#' @param patientID (charachter) the columns name containing the patient ID. Default is "pID"
+#' @param hospitalID (charachter) the columns name containing the hospital ID. Default is "hID"
+#' @param admDate (charachter) the columns name containing the admission date. Default is "Adate"
+#' @param disDate (charachter) the columns name containing the discharge date. Default is "Ddate"
+#' 
 #' @return The corrected database as data.table.
 #' @export
 #' 
@@ -30,18 +35,30 @@ checkDates<-function(base,
                      bestGuess=FALSE,
                      overnight=FALSE,
                      deleteErrors="",
+                     patientID="pID",
+                     hospitalID="hID",
+                     disDate="Ddate",
+                     admDate="Adate",
                      ...){
+
   #Check if the columns for discharge and admission date are present
   disMissing<-FALSE
   admMissing<-FALSE
-  if(!("admission" %in% colnames(base))) admMissing<-TRUE
-  if(!("discharge" %in% colnames(base))) disMissing<-TRUE
-  if(disMissing&admMissing) stop("Both admission and discharge date varialbe are missing. Make sure they are called 'admission' and 'discharge' ")
-  if(disMissing) stop("Discharge date varialbe is missing. Make sure it is called 'discharge' ")
-  if(admMissing) stop("Admission date varialbe is missing. Make sure it is called 'admission' ")
+  if(!(admDate %in% colnames(base))) admMissing<-TRUE
+  if(!(disDate %in% colnames(base))) disMissing<-TRUE
+  if(disMissing&admMissing) stop(paste0("Both admission and discharge date variables are missing, expecting columns called ",admDate," and ",disDate,".\nPlease supply column names in options admDate=\"...\" and disDate=\"...\". "))
+  if(disMissing) stop(paste0("Discharge date variable is missing, expecting column called ",disDate,".\nPlease supply column name in option disDate=\"...\". "))
+  if(admMissing) stop(paste0("Admission date variable is missing, expecting column called ",admDate,".\nPlease supply column name in option admDate=\"...\". "))
+  if(!(patientID %in% colnames(base))) stop(paste0("patient ID variable is missing, expecting column called ",admDate,".\nPlease supply column name in option patientID=\"...\". "))
+  if(!(hospitalID %in% colnames(base))) stop(paste0("Hospital ID variable is missing, expecting column called ",admDate,".\nPlease supply column name in option hospitalID=\"...\". "))
+  
+  colnames(base)[colnames(base)==patientID] <- "pID"
+  colnames(base)[colnames(base)==hospitalID] <- "hID"
+  colnames(base)[colnames(base)==admDate] <- "Adate"
+  colnames(base)[colnames(base)==disDate] <- "Ddate"
   
   #Check if discharge and admission dates are formatted as POSIXct dates
-  needsConverting<-((!is.POSIXct(base$discharge))|(!is.POSIXct(base$admission)))
+  needsConverting<-((!is.POSIXct(base$Ddate))|(!is.POSIXct(base$Adate)))
   
   if(needsConverting&!convertDates) stop("admission and/or discharge date not in POSIXct format\n Use convertDates=TRUE option to convert dates")
   
@@ -57,12 +74,12 @@ checkDates<-function(base,
       }
       testFormats<-c("ymd","ydm","dmy","dym","mdy","myd")
       #Guess the format for the admissions dates
-      guesses<-as.data.frame(lapply(testFormats,function(x){suppressWarnings(parse_date_time(sampleData$admission,x))}),col.names=testFormats)
+      guesses<-as.data.frame(lapply(testFormats,function(x){suppressWarnings(parse_date_time(sampleData[,Adate],x))}),col.names=testFormats)
       naCount<-apply(guesses, 2, function(x){sum(is.na(x))})
       admFormat<-testFormats[order(naCount)[1]]
       
       #Guess the format for the discharge dates
-      guesses<-as.data.frame(lapply(testFormats,function(x){suppressWarnings(parse_date_time(sampleData$discharge,x))}),col.names=testFormats)
+      guesses<-as.data.frame(lapply(testFormats,function(x){suppressWarnings(parse_date_time(sampleData[,Ddate],x))}),col.names=testFormats)
       naCount<-apply(guesses, 2, function(x){sum(is.na(x))})
       disFormat<-testFormats[order(naCount)[1]]
       
@@ -75,41 +92,40 @@ checkDates<-function(base,
     
     # start the actual converting, if the format is given.
     if(admFormat==""||disFormat=="") {stop("No date format giving for date conversion.\nPlease provide date format using option (e.g.) dateFormat=\"dmy\", \nor use option bestGuess=TRUE to guess best date format.")}
-   
     print("Converting dates to POSIXct format. This may take a while...")
-    base$admission<-parse_date_time(base$admission,admFormat)
-    base$discharge<-parse_date_time(base$discharge,disFormat)
+    base$Ddate<-parse_date_time(base$Ddate,disFormat)
+    base$Adate<-parse_date_time(base$Adate,admFormat)
   }
   
   # Check if there are records with NA in admission or discharge field, and delete them as given in function options
-  withNA<-subset(base,is.na(discharge)|is.na(admission))
+  withNA<-subset(base,is.na(Adate)|is.na(Ddate))
   if(nrow(withNA)>0){
     print(paste0("Found ",nrow(withNA)," records with NA in admission and/or discharge date")) 
     if(deleteErrors=="record"){
       nrBefore<-nrow(base)
-      base<-subset(base,!(is.na(discharge)|is.na(admission)))
+      base<-subset(base,!(is.na(Adate)|is.na(Ddate)))
       print(paste0("- Deleted ",nrBefore-nrow(base)," incorrect records"))
     }
     if(deleteErrors=="patient"){
       nrBefore<-nrow(base)
-      base<-subset(base,!(patientID %in% withNA$patientID))
+      base<-subset(base,!(pID %in% withNA$pID))
       print(paste0("- Deleted patients with incorrect records, deleted ",nrBefore-nrow(base)," records"))
     }
     if(deleteErrors==""){stop(paste0("Found ",nrow(withNA)," records with discharge before admission.\n Use deleteErrors==\"record\" to delete the incorrect records, or deleteErrors==\"patient\" to delete all patients with incorrect records." ))}
   }
   
   # Check if there are records with discharge before admission in admission or discharge field, and delete them as given in function options  
-  wrongOrder<-subset(base,discharge<admission)
+  wrongOrder<-subset(base,Ddate<Adate)
   if(nrow(wrongOrder)>0){
     print(paste0("Found ",nrow(wrongOrder)," records with discharge before admission"))  
     if(deleteErrors=="record"){
       nrBefore<-nrow(base)
-      base<-subset(base,discharge>=admission)
+      base<-subset(base,Ddate>=Adate)
       print(paste0("- Deleted ",nrBefore-nrow(base)," incorrect records"))
     }
     if(deleteErrors=="patient"){
       nrBefore<-nrow(base)
-      base<-subset(base,!(patientID %in% wrongOrder$patientID))
+      base<-subset(base,!(pID %in% wrongOrder$pID))
       print(paste0("- Deleted patients with incorrect records, deleted ",nrBefore-nrow(base)," records"))
     }
     if(deleteErrors==""){stop(paste0("Found ",nrow(wrongOrder)," records with discharge before admission.\n Use deleteErrors==\"record\" to delete the incorrect records, or deleteErrors==\"patient\" to delete all patients with incorrect records." ))}
@@ -118,7 +134,7 @@ checkDates<-function(base,
   # Delete single day cases if only overnight patients are defined
   if(overnight){
     nrBefore<-nrow(base)
-    base<-subset(base,admission<discharge)
+    base<-subset(base,Adate<Ddate)
     print(paste0("Deleted ",nrBefore-nrow(base)," patient stay records who did not stay overnight"))
   }
   
