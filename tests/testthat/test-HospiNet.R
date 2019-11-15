091e26462503dc8622f60bb136d8911137ec21cd
Pascal Crépey <crepey@gmail.com>
2019-11-15 10:31:43 +0100

    - cover plot of hist degree in test - fix documentation for HospiNet (some fields are not implemented yet

--- tests/testthat/test-HospiNet.R
+++ tests/testthat/test-HospiNet.R
@@ -7,13 +7,14 @@ test_that("plotting functions on non-clustered network",{
   checked_db = checkBase(db)
   
   hn = hospinet_from_subject_database(checked_db)
-  
+  plot_deg = plot(hn, type = "degree")
   plot_mat100 = plot(hn)
   plot_clust100 = plot(hn, type = "clustered_matrix")
   #plot_circular_network100 = plot(hn, type = "circular_network")
   
   vdiffr::expect_doppelganger("Matrix plot 100", plot_mat100)
   vdiffr::expect_doppelganger("Clustered Matrix plot 100", plot_clust100)
+  vdiffr::expect_doppelganger("plot degree 100", plot_deg)
   
 })
 #vdiffr::expect_doppelganger("Circular plot 100", plot_circular_network100)
@@ -26,13 +27,13 @@ test_that("plotting functions on clustered network", {
   hn = hospinet_from_subject_database(checked_db)
   
   plot_mat_net500 = plot(hn)
+  plot_deg_net500 = plot(hn, type = "degree")
   plot_clust_net500 = plot(hn, type = "clustered_matrix")
   plot_circular_clust_network500 = plot(hn, type = "circular_network")
   
   vdiffr::expect_doppelganger("Matrix plot clust 500", plot_mat_net500)
   vdiffr::expect_doppelganger("Clustered Matrix plot clust 500", plot_clust_net500)
-  
-
+  vdiffr::expect_doppelganger("plot clust degree 500", plot_deg_net500)
   vdiffr::expect_doppelganger("Circular plot clust 500", plot_circular_clust_network500)
   
 })
