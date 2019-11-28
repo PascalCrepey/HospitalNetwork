0fb9bac3829f6145ed4f3113b5ee8d8b137d3366
Pascal Crépey <crepey@gmail.com>
2019-11-28 17:32:44 +0100

    - fix: shortened paths for svg figures

--- tests/testthat/test-HospiNet.R
+++ tests/testthat/test-HospiNet.R
@@ -1,7 +1,7 @@
-context("HospiNet object - plotting functions")
+context("HospiNet plotting")
 
 #' @importFrom vdiffr expect_doppelganger
-test_that("plotting functions on non-clustered network",{
+test_that("plotting non-clustered network",{
   set.seed(42)
   db = create_fake_subjectDB(n_subjects = 100, with_errors = FALSE)
   checked_db = checkBase(db)
@@ -12,14 +12,14 @@ test_that("plotting functions on non-clustered network",{
   plot_clust100 = plot(hn, type = "clustered_matrix")
   #plot_circular_network100 = plot(hn, type = "circular_network")
   
-  vdiffr::expect_doppelganger("Clustered Matrix plot 100", plot_clust100)
-  vdiffr::expect_doppelganger("plot degree 100", plot_deg)
-  #vdiffr::expect_doppelganger("Matrix plot 100", plot_mat100)
+  vdiffr::expect_doppelganger("ClMat100", plot_clust100)
+  vdiffr::expect_doppelganger("Deg100", plot_deg)
+  vdiffr::expect_doppelganger("Mat100", plot_mat100)
   
 })
 #vdiffr::expect_doppelganger("Circular plot 100", plot_circular_network100)
 
-test_that("plotting functions on clustered network", {
+test_that("plotting clustered network", {
   set.seed(42)
   db = create_fake_subjectDB_clustered(n_subjects = 500, n_facilities = 30)
   checked_db = checkBase(db)
@@ -31,10 +31,10 @@ test_that("plotting functions on clustered network", {
   plot_clust_net500 = plot(hn, type = "clustered_matrix")
   plot_circular_clust_network500 = plot(hn, type = "circular_network")
   
-  vdiffr::expect_doppelganger("Clustered Matrix plot clust 500", plot_clust_net500)
-  vdiffr::expect_doppelganger("plot clust degree 500", plot_deg_net500)
-  vdiffr::expect_doppelganger("Circular plot clust 500", plot_circular_clust_network500)
-  #vdiffr::expect_doppelganger("Matrix plot clust 500", plot_mat_net500)
+  vdiffr::expect_doppelganger("ClustMatClust500", plot_clust_net500)
+  vdiffr::expect_doppelganger("ClustDeg500", plot_deg_net500)
+  vdiffr::expect_doppelganger("CircClust500", plot_circular_clust_network500)
+  vdiffr::expect_doppelganger("MatClust500", plot_mat_net500)
   
 })
 
