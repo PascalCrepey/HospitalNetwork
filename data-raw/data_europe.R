8d97fb94169190bc7b5b2fe01581820db240f93c
jroux <jonathan.roux@santepubliquefrance.fr>
2023-02-07 16:14:08 +0000

    Add dataset of European data in data

--- data-raw/data_europe.R
+++ data-raw/data_europe.R
@@ -5,13 +5,21 @@ library(ggplot2)
 
 
 ## Loading of GPS coordinates and capacity beds of hospitals ##
-data_europe = fread("./inst/extdata/heltcareCsv/all.csv")
-summary(data)
-usethis::use_data_raw("data_europe")
+data_europe = fread("./inst/extdata/healthcareCsv/all.csv")
+summary(data_europe)
+# usethis::use_data_raw("data_europe")
 
-data$beds <- rpois(data[,.N], lambda= 200) #imputation of the nb od beds sinc we had a lot of NA in the initial beds info
+# Imputation of the nb of beds since we had a lot of NA in the initial beds info #
+data_europe$beds = rpois(data_europe[,.N], lambda= 200) 
 
-usethis::use_data(data_europe, overwrite = TRUE)
+# Cleaning just to keep essential information for final dataset #
+dataset_europe = data_europe[,.(id, country,
+                                lat = as.numeric(lat), 
+                                long = as.numeric(lon),
+                                beds,
+                                beds_real_number = cap_beds)]
+
+usethis::use_data(dataset_europe, overwrite = TRUE)
 
 
 
