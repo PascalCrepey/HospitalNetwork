dc89595e2d6d18924f93839e6c64ebd25ff34168
jroux <jonathan.roux@santepubliquefrance.fr>
2023-02-15 16:59:12 +0000

    Allowing the loading of GPS data for real data

--- R/mod_loadNcheck.R
+++ R/mod_loadNcheck.R
@@ -22,31 +22,11 @@ mod_loadNcheck_ui <- function(id) {
       tabPanel(title = "Upload and check dataset",
                fileInput(ns("dataset"),
                          "File",
-                         buttonLabel = HTML(paste(icon("upload",  ), "Browse"))),
-               fluidRow(
-                 column(5, HTML("<b>File format</b>")),
-                 column(7, 
-                        shinyWidgets::radioGroupButtons(ns("filetype"),
-                                                        choices = c("CSV", "Rdata", "RDS"),
-                                                        status = "default",
-                                                        size = "sm",
-                                                        selected = NA)
-                 )
-               )
-               # fileInput(ns("geoloc_dataset"),
-               #           "File with GPS coord. and beds",
-               #           buttonLabel = HTML(paste(icon("upload",  ), "Browse"))),
-               # uiOutput(ns("checkUI")),
-               # fluidRow(
-               #   column(5, HTML("<b>File format for GPS</b>")),
-               #   column(7, 
-               #          shinyWidgets::radioGroupButtons(ns("geoloc_filetype"),
-               #                                          choices = c("CSV", "Rdata", "RDS"),
-               #                                          status = "default",
-               #                                          size = "sm",
-               #                                          selected = NA)
-               #   )
-               # )
+                         buttonLabel = "Browse"),
+               checkboxInput(ns("gps_load"), HTML("<b>Add dataset with GPS coordinates and bed capacities</b>")),
+               uiOutput(ns("gps_loadUI")),
+               actionButton(ns("loadfiles"), "Load files", icon = icon("upload")),
+               uiOutput(ns("checkUI"))
       ),
       tabPanel(title = "Fake dataset", 
                sliderInput(ns("fd_n_subjects"), "Number of subjects", 
@@ -79,77 +59,20 @@ mod_loadNcheck_ui <- function(id) {
 #' @rdname mod_loadNcheck
 #' @export
 #' @keywords internal
+#' @importFrom openxlsx read.xlsx
+#' @importFrom readxl read_xls
     
 mod_loadNcheck_server <- function(input, output, session, parent, mainData){
     ns <- session$ns
 
     ## Reactive value to store the checked database
     base = reactiveVal()
-    # geoloc_dataset = reactiveVal()
+    # gps_dataset = reactiveVal()
     
     output$intro_md <- renderUI({
       includeMarkdown("inst/intro.md")
     })
 
-    #--- Load files ----------------------------------------------------    
-    dataset = reactive({
-        req(input$dataset)
-    })
-    output$fileUploaded <- reactive({
-        return(!is.null(dataset()))
-    })
-    outputOptions(output, 'fileUploaded', suspendWhenHidden=FALSE)
-    
-    # geoloc_dataset = reactive({
-    #   req(input$geoloc_dataset)
-    # })
-    # output$fileUploaded_geoloc <- reactive({
-    #   return(!is.null(geoloc_dataset()))
-    # })
-    # outputOptions(output, 'fileUploaded_geoloc', suspendWhenHidden=FALSE)
-    # 
-    #where we put the main datatable
-    datatable = reactiveVal(NULL)
-    #--- Read file depending on type ----------------------------------
-    observeEvent(input$filetype,{
-      req(input$filetype)
-      #reset the base
-      base(NULL)
-      user_file = dataset()$datapath
-      if (input$filetype == "CSV") {
-        datatable(fread(user_file))
-      }
-      else if (input$filetype == "Rdata") {
-          e = new.env()
-          dtb = load(user_file, envir = e)
-          datatable(e[[dtb]])
-      }
-      else {
-        datatable(readRDS(user_file))
-      }
-    })
-    
-    # #Same for GPS coordinates and beds
-    # geoloc_datatable = reactiveVal(NULL)
-    # #--- Read file depending on type ----------------------------------
-    # observeEvent(input$geoloc_filetype,{
-    #   req(input$geoloc_filetype)
-    #   #reset the base
-    #   geoloc_dataset(NULL)
-    #   geoloc_user_file = geoloc_dataset()$datapath
-    #   if (input$filetype == "CSV") {
-    #     datatable(fread(geoloc_user_file))
-    #   }
-    #   else if (input$geoloc_filetype == "Rdata") {
-    #     e = new.env()
-    #     dtb = load(geoloc_user_file, envir = e)
-    #     datatable(e[[dtb]])
-    #   }
-    #   else {
-    #     datatable(readRDS(geoloc_user_file))
-    #   }
-    # })
-    
     # Generate the fake data ----
     observeEvent(input$buildFD, {
       withProgress(message = "Building fake data...",{
@@ -183,7 +106,7 @@ mod_loadNcheck_server <- function(input, output, session, parent, mainData){
                                                                input$fd_n_subjects,"/",
                                                                input$fd_n_facilities,"/",
                                                                input$fd_n_clusters))
-
+      
       shinyalert::shinyalert(title = "Fake database built successfully",
                              text = paste0("The database containing ",
                                            input$fd_n_subjects, " subjects, ",
@@ -192,17 +115,91 @@ mod_loadNcheck_server <- function(input, output, session, parent, mainData){
                              type = "success")
       
       shinyjs::show(id = "downloadNgoto")
+      
+    })
+    
+    #--- Real data from the user ----------------------------------------------------    
+    dataset = reactive({
+        req(input$dataset)
+    })
+    output$fileUploaded <- reactive({
+        return(!is.null(dataset()))
+    })
+    outputOptions(output, 'fileUploaded', suspendWhenHidden=FALSE)
 
+    gps_dataset = reactive({
+      req(input$gps_dataset)
+    })
+    output$fileUploaded_gps <- reactive({
+      return(!is.null(gps_dataset()))
+    })
+    outputOptions(output, 'fileUploaded_gps', suspendWhenHidden=FALSE)
+
+    
+    #--- Loading the datasets provided by the user ---------------------------------
+    datatable = reactiveVal(NULL)
+    datatable_gps = reactiveVal(NULL)
+    # Read file depending on type #
+    observeEvent(input$loadfiles,{
+      req(input$loadfiles)
+      #reset the base
+      base(NULL)
+      # Hospital stays
+      user_file = dataset()$datapath
+      extension_file = unlist(strsplit(user_file, split = "\\."))[2]
+      if (extension_file == "csv") {
+        datatable(fread(user_file))
+      }
+      else if (extension_file == "xlsx") {
+        datatable(as.data.table(read.xlsx(user_file)))
+      }
+      else if (extension_file == "xls") {
+        datatable(as.data.table(read_xls(user_file)))
+      }
+      else if (extension_file %in% c("Rdata", "RData")) {
+          e = new.env()
+          dtb = load(user_file, envir = e)
+          datatable(e[[dtb]])
+      }
+      else {
+        datatable(readRDS(user_file))
+      } # Add an error if not one of xlsx, xls, Rdata, RData, rds or csv
+      
+      if(input$gps_load){
+        # GPS and beds data
+        gps_user_file = gps_dataset()$datapath
+        gps_extension_file = unlist(strsplit(gps_user_file, split = "\\."))[2]
+        if (gps_extension_file == "csv") {
+          datatable_gps(fread(gps_user_file))
+        }
+        else if (gps_extension_file == "xlsx") {
+          datatable_gps(as.data.table(read.xlsx(gps_user_file)))
+        }
+        else if (gps_extension_file == "xls") {
+          datatable_gps(as.data.table(read_xls(gps_user_file)))
+        }
+        else if (gps_extension_file %in% c("Rdata", "RData")) {
+          e = new.env()
+          dtb = load(gps_user_file, envir = e)
+          datatable_gps(e[[dtb]])
+        }
+        else {
+          datatable_gps(readRDS(gps_user_file))
+        } # Add an error if not one of xlsx, xls, Rdata, RData, rds or csv
+      }
+        
+    })
+    
+    previously_checked = eventReactive(input$loadfiles, {
+        inherits(base(), "hospinet.base")
     })
 
-    previously_checked = eventReactive(input$filetype, {
-        inherits(datatable(), "hospinet.base")
-    })
     
     ##--- Conditional UI to check database -----------------------------------
     output$checkUI = renderUI({
-        req(input$filetype)
+        req(input$loadfiles)
         req(input$dataset)
+        # Transfer dataset #
         validate(need(class(try(datatable())) != "try-error",
                       message = "Error reading file. Please verify file format"))
         validate(need("data.frame" %in% class(datatable()),
@@ -210,10 +207,20 @@ mod_loadNcheck_server <- function(input, output, session, parent, mainData){
         validate(need(ncol(datatable()) >= 4,
                       message = "Error: dataset must have at least 4 columns"))
         cols = colnames(datatable())
-        conditionalUI = if (input$filetype == "CSV" |
-                            (input$filetype == "Rdata" & !previously_checked()) |
-                            (input$filetype == "RDS" & !previously_checked())) {
+        # GPS data #
+        if(input$gps_load){
+          validate(need(class(try(datatable_gps())) != "try-error",
+                        message = "Error reading file. Please verify file format"))
+          validate(need("data.frame" %in% class(datatable_gps()),
+                        message = "Error: data provided is not a data.frame or data.table"))
+          validate(need(ncol(datatable_gps()) >= 4,
+                        message = "Error: dataset must have at least 4 columns"))
+          cols_gps = colnames(datatable_gps())
+        }
+        
+        conditionalUI = if (!previously_checked() & !input$gps_load) {
                             list(
+                              HTML("<b>Columns check for file\n</b>"),
                                 fluidRow(
                                     column(5, HTML("<b>Subject identifier:</b>")),
                                     column(7, selectInput(ns("sID"),
@@ -275,10 +282,104 @@ mod_loadNcheck_server <- function(input, output, session, parent, mainData){
                                              choices = c("No" = "No",
                                                          "Remove record" = "record",
                                                          "Remove patient" = "patient"),
-                                             inline = T)
-                            )
+                                             inline = T))
+                        } else if(!previously_checked()) {
+                          list(
+                            HTML("<b>\nColumns check for file\n</b>"),
+                            fluidRow(
+                              column(5, HTML("<b>Subject identifier:</b>")),
+                              column(7, selectInput(ns("sID"),
+                                                    label = NULL,
+                                                    choices = cols,
+                                                    selected = cols[1]))
+                            ),
+                            fluidRow(
+                              column(5, HTML("<b>Facility identifier:</b>")),
+                              column(7, selectInput(ns("fID"),
+                                                    label = NULL,
+                                                    choices = cols,
+                                                    selected = cols[2]))
+                            ),
+                            fluidRow(
+                              column(5, HTML("<b>Admission date</b>")),
+                              column(7, selectInput(ns("adate"),
+                                                    label = NULL,
+                                                    choices = cols,
+                                                    selected = cols[3]))
+                            ),
+                            fluidRow(
+                              column(5, HTML("<b>Discharge date</b>")),
+                              column(7, selectInput(ns("ddate"),
+                                                    label = NULL,
+                                                    choices = cols,
+                                                    selected = cols[4]))
+                            ),
+                            fluidRow(
+                              column(5, HTML("<b>Date format</b>")),
+                              column(7, selectInput(ns("dateformat"),
+                                                    label = NULL,
+                                                    choices = c("Year-Month-Day" = "ymd",
+                                                                "Year-Day-Month" = "ydm",
+                                                                "Month-Day-Year" = "mdy",
+                                                                "Day-Month-Year" = "dmy",
+                                                                "Day-Year-Month" = "dym",
+                                                                "Month-Year-Day" = "myd"),
+                                                    selected = "Year-Month-Day"))
+                            ),
+                            fluidRow(
+                              column(5, HTML("<b>Time format</b>")),
+                              column(7, selectInput(ns("timeformat"),
+                                                    label = NULL,
+                                                    choices = c("None" = "",
+                                                                "Hours-Minutes-Seconds" = "HMS",                                                                    "Hours-Minutes-Seconds" = "HMS",
+                                                                "Seconds-Minutes-Hours" = "SMH",
+                                                                "Hours-Minutes" = "HM"),
+                                                    selected = "None"))
+                            ),
+                            shinyWidgets::awesomeRadio(ns("deletemissing"),
+                                                       "Remove missing values?",
+                                                       choices = c("No" = "No",
+                                                                   "Remove record" = "record",
+                                                                   "Remove patient" = "patient"),
+                                                       inline = T),
+                            shinyWidgets::awesomeRadio(ns("deleteerrors"),
+                                                       "Remove erroneous records?",
+                                                       choices = c("No" = "No",
+                                                                   "Remove record" = "record",
+                                                                   "Remove patient" = "patient"),
+                                                       inline = T),
+                            HTML("<b>Columns check for GPS file\n</b>"),
+                            fluidRow(
+                              column(5, HTML("<b>Facility identifier:</b>")),
+                              column(7, selectInput(ns("fID"),
+                                                    label = NULL,
+                                                    choices = cols_gps,
+                                                    selected = cols_gps[1]))
+                            ),
+                            fluidRow(
+                              column(5, HTML("<b>Latitude:</b>")),
+                              column(7, selectInput(ns("lat"),
+                                                    label = NULL,
+                                                    choices = cols_gps,
+                                                    selected = cols_gps[2]))
+                            ),
+                            fluidRow(
+                              column(5, HTML("<b>Longitude</b>")),
+                              column(7, selectInput(ns("long"),
+                                                    label = NULL,
+                                                    choices = cols_gps,
+                                                    selected = cols_gps[3]))
+                            ),
+                            fluidRow(
+                              column(5, HTML("<b>Number of beds</b>")),
+                              column(7, selectInput(ns("beds"),
+                                                    label = NULL,
+                                                    choices = cols_gps,
+                                                    selected = cols_gps[4]))
+                            ))
+                            
                         } else {
-                            NULL
+                          NULL
                         }
         startUI = list(
             div(style="display: inline-block;vertical-align:top;",
@@ -290,6 +391,14 @@ mod_loadNcheck_server <- function(input, output, session, parent, mainData){
         )
         return(conditionalPanel(paste0("output['", ns("fileUploaded"), "']"),
                                 c(conditionalUI, startUI)))
+    })
+    
+    ##--- Conditional UI to load GPS data -----------------------------------
+    output$gps_loadUI = renderUI({
+      req(input$gps_load)
+      fileInput(ns("gps_dataset"),
+                "File with GPS coord. and beds",
+                buttonLabel = "Browse")
     })
     
     output$downloadNgoto <- renderUI({
@@ -315,7 +424,8 @@ mod_loadNcheck_server <- function(input, output, session, parent, mainData){
         base(NULL)
         if (!previously_checked()) {
         out = withCallingHandlers(
-            try(HospitalNetwork::checkBase(base = datatable(),
+            try(HospitalNetwork::checkBase(base = list(all_s_stays = datatable(),
+                                                       gps_facilities = datatable_gps()),
                                            convertDates = T,
                                            dateFormat = args()$datetimeformat,
                                            deleteMissing = args()$deleteMissing,
